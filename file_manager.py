c66b95e2785b24275489a0376a289c988c1eb8bf
IgorBrat <akbars5678@gmail.com>
2022-06-06 23:35:03 +0300

    Updated readme and added printing count of regex matches

--- file_manager.py
+++ file_manager.py
@@ -4,12 +4,16 @@ from typing import List
 
 class FileManager:
     """Class that works with files"""
+
     @staticmethod
     def print_edu_bug(location: str):
         regex = r'bugs.*\.edu.*menu.*'
+        count = 0
         with open(location) as file:
             lines = file.readlines()
             for line in lines:
                 find_regex = re.search(regex, line)
                 if find_regex is not None:
                     print(find_regex.group())
+                    count += 1
+        print(f"Count of matches: {count}")
