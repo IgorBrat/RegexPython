cdc8cd9e6bc2e22f5f400d9e2864732cc1412b09
IgorBrat <akbars5678@gmail.com>
2022-06-07 17:37:10 +0300

    Added managing zip files

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ def main():
     location = os.path.abspath(file_name)
     FileManager.print_edu_bug(location=location)
 
+    # zip_name = "con228.bugs.zip"
+    # location = os.path.abspath(zip_name)
+    # FileManager.manage_zip(location=location, filename=file_name)
+
 
 if __name__ == "__main__":
     main()
